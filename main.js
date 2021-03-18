420ee8be90a749c52c46ce2b60a353a41dfa07e3
Rafael Miranda <58390974+Rafa84@users.noreply.github.com>
2021-03-18 15:30:39 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,67 @@
+
+function clicou(){
+    document.getElementById("agradecimento").innerHTML="<b>Obrigado por clicar</b>";
+    
+    //alert("Obrigado por clicar");
+}
+
+function redirecionar(){
+    window.open("https://www.google.com/");
+}
+
+
+/*function soma(n1,n2){
+    return n1+n2;
+}
+
+function setreplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome);
+}
+
+alert(soma(5,10));
+alert(setreplace("Vai Japão", "Japão", "Brasil"));
+
+/*
+var d = new Date();
+alert(d.getMonth()+1);
+alert(d.getDay());
+*/
+
+/*
+
+var count;
+for(count =0; count<=5; count++){
+    alert(count);
+};
+/*
+/*
+var count = 0;
+while (count <=5){
+    console.log(count);
+    count++;
+};
+
+/*
+var n1 = prompt("Qual a sua idade?");
+if( n1 > 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+};
+*/
+
+//var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"verde"}];
+//console.log(frutas);
+//alert(frutas[1].nome);
+
+//var lista = ["maça", "pera", "laranja"];
+//lista.push("uva");
+//console.log(lista.reverse());
+
+//var nome = "Rafael Miranda";
+//var idade = 36;
+//var frase = "Japão é o melhor time do mundo"
+//alert(nome+" tem "+idade+" anos");
+//console.log(nome);
+//console.log(idade);
+//console.log(frase.replace("Japão", "Argentina"));
